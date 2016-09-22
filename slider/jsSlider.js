5467e1de97805f01a4f4bfd5eb997a9184934fd5
bigmickeystyle <mike.123.smith@gmail.com>
2016-09-22 17:16:50 +0200

    now resizable

--- slider/jsSlider.js
+++ slider/jsSlider.js
@@ -16,9 +16,37 @@ $('#bar').on('mousedown', function(){
             var cropper = $('#bar').css('left');
             $('#image2').css({width: cropper});
         }
+        console.log("cropper" + cropper);
         $(document).on('mouseup.remove', function(){
             $(this).off('.remmove');
             $(this).off('.remove');
+        });
+    });
+});
+
+var recordx = 0;
+
+$('#arrow').on('mousedown', function(){
+    $(document).on('mousemove.arrowmove', function(e){
+        e.preventDefault();
+        var livebar = parseInt($('#bar').css('left'), 10);
+        var movementX = e.pageX;
+        var movementY = e.pageY;
+        $('#image1').css({width: movementX, height: movementY});
+        if(livebar >= recordx) {
+            $('#bar').css({height: movementY, left: movementX});
+            $('#image2').css({width: movementX, height: movementY});
+        }
+        else{
+            $('#bar').css({height: movementY});
+            $('#image2').css({width: livebar, height: movementY});
+        }
+        $('#arrow').css({top: movementY - 10, left: movementX - 10});
+        recordx = movementX;
+
+        $(document).on('mouseup.arrowstop', function(){
+            $(this).off('.arrowmove');
+            $(this).off('.arrowstop');
         });
     });
 });
